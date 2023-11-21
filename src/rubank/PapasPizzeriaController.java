c34df61f69794a38db602e62006007ba05e9a943
rrahulraj2003 <rrahulraj2003@gmail.com>
2023-11-20 21:09:27 -0500

    upload

---
+++ src/rubank/PapasPizzeriaController.java
@@ -0,0 +1,16 @@
+package rubank;
+
+import javafx.fxml.FXML;
+
+
+/**
+ * The controller class for Transaction Manager, defining functionalities for all buttons and Text Fields on the GUI
+ *
+ * @author Sibi suriyanarayan Tiruchirapalli venketaramani, Rahulraj Rajesh
+ */
+public class PapasPizzeriaController {
+    @FXML
+    void open() { //depositing into account not in database
+        System.out.println("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+    }
+}
---
+++ src/rubank/PapasPizzeriaMain.java
@@ -0,0 +1,27 @@
+package rubank;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PapasPizzeriaMain extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(PapasPizzeriaMain.class.getResource("PapasPizzeriaView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),750,750);
+        primaryStage.setResizable(false);
+        primaryStage.setHeight(650);
+        primaryStage.setWidth(617);
+        primaryStage.setTitle("Papa's Pizzeria");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
