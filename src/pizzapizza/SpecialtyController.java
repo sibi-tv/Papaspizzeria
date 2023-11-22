2168a1e368edaa8eef721cd1b1f9974e5fa474a5
rrahulraj2003 <rrahulraj2003@gmail.com>
2023-11-22 11:47:31 -0500

    updated buttons and added fxids

--- src/pizzapizza/SpecialtyController.java
+++ src/pizzapizza/SpecialtyController.java
@@ -3,7 +3,10 @@ package pizzapizza;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +14,15 @@ import java.util.ResourceBundle;
 public class SpecialtyController implements Initializable {
     @FXML
     ComboBox<String> combobox;
+
+    @FXML
+    Label totalprice;
+
+    @FXML
+    CheckBox extraextra;
+
+    @FXML
+    ListView toppingslist;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
